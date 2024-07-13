c384d818cb07011e9793eb37d71e24f332d12473
Marlen272 <159940678+Marlen272@users.noreply.github.com>
2024-07-13 17:47:05 +0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import PokemonList from './components/PokemonList';
+import  './components/PokemonList.css'
+
+function App() {
+  return (
+      <div className="h1">
+        <h1>Pokemon List</h1>
+        <PokemonList />
+      </div>
+  );
+}
+
+export default App;
